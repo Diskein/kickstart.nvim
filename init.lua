b8c697b78c5a3afd70bcecfba5fa2d5f57e76f15
Denis K <kalantaevskii@gmail.com>
2025-07-14 21:54:34 +0200

    disable snippets
    They are annoying because they appear in autocomplete on the top of its
    list

--- init.lua
+++ init.lua
@@ -800,14 +800,14 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load {
-                include = { 'cpp', 'c', 'python', 'shell', 'rust' },
-              }
-            end,
-          },
+          -- {
+          --   'rafamadriz/friendly-snippets',
+          --   config = function()
+          --     require('luasnip.loaders.from_vscode').lazy_load {
+          --       include = { 'cpp', 'c', 'python', 'shell', 'rust' },
+          --     }
+          --   end,
+          -- },
         },
         opts = {},
       },
