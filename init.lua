2e332d864c1f822d2ff963294bf4a04cf6940183
Den Kalantaevskii <den.kalantaevskii@clickhouse.com>
2026-05-07 22:53:02 +0200

    replace some telescope command with fzf-lua

--- init.lua
+++ init.lua
@@ -428,7 +428,7 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      -- vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -1062,6 +1062,22 @@ require('lazy').setup({
       -- Diff management
       { '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'Accept diff' },
       { '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'Deny diff' },
+    },
+    {
+    'ibhagwan/fzf-lua',
+    -- optional for icon support
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "nvim-mini/mini.icons" },
+    ---@module "fzf-lua"
+    ---@type fzf-lua.Config|{}
+    ---@diagnostic disable: missing-fields
+    opts = {},
+    ---@diagnostic enable: missing-fields
+    keys = {
+      { '<leader>sf', '<cmd>FzfLua files<cr>', desc = 'Fzf Files' },
+      { '<leader>sg', '<cmd>FzfLua live_grep<cr>', desc = 'Fzf Grep' },
+      { '<leader>sj', '<cmd>FzfLua lsp_finder<cr>', desc = 'Fzf ALL Lsp' },
     },
   },
 }, {
